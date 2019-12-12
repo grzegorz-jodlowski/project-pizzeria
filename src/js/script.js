46b7773836f564a0bbfb6bd494bc40f433ba7270
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2019-12-12 16:54:21 +0100

    Add function remove to CartProduct class

--- src/js/script.js
+++ src/js/script.js
@@ -465,6 +465,20 @@
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
     }
+
+
+    remove() {
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
   }
 
   const app = {

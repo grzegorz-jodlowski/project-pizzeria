f0b7655efbe3543d8ed6d244508b9221840f5556
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-01-06 13:51:35 +0100

    Delete use strict mode from app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,57 +1,51 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+const app = {
+  initMenu: function () {
+    const thisApp = this;
+    console.log('thisApp.data:', thisApp.data);
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+  initData: function () {
+    const thisApp = this;
 
-{
-  'use strict';
+    thisApp.data = {};
 
-  const app = {
-    initMenu: function () {
-      const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
-      for (let productData in thisApp.data.products) {
-        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-      }
-    },
-    initData: function () {
-      const thisApp = this;
+    const url = settings.db.url + '/' + settings.db.product;
 
-      thisApp.data = {};
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
 
-      const url = settings.db.url + '/' + settings.db.product;
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
+      });
+    console.log('aaathisApp.data', JSON.stringify(thisApp.data));
 
-      fetch(url)
-        .then(function (rawResponse) {
-          return rawResponse.json();
-        })
-        .then(function (parsedResponse) {
-          console.log('parsedResponse', parsedResponse);
+  },
 
-          thisApp.data.products = parsedResponse;
-          thisApp.initMenu();
-        });
-      console.log('aaathisApp.data', JSON.stringify(thisApp.data));
+  initCart: function () {
+    const thisApp = this;
 
-    },
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+    console.log(' : thisApp.cart');
+  },
 
-    initCart: function () {
-      const thisApp = this;
+  init: function () {
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
 
-      const cartElem = document.querySelector(select.containerOf.cart);
-      thisApp.cart = new Cart(cartElem);
-      console.log(' : thisApp.cart');
-    },
+    thisApp.initData();
+    thisApp.initCart();
+  },
+};
 
-    init: function () {
-      const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-
-      thisApp.initData();
-      thisApp.initCart();
-    },
-  };
-
-  app.init();
-}
+app.init();

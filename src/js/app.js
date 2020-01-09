242c92bda68e363b4f3a3ae1bff12ac690a3a3f6
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-01-09 18:41:24 +0100

    Add structure of app.initPages function

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,14 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    thisApp.activatePage(thisApp.pages[0]);
+  },
+
   initMenu: function () {
     const thisApp = this;
     console.log('thisApp.data:', thisApp.data);

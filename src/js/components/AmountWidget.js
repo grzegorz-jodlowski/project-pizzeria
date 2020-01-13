7bf8d310ea6be99f0b0bf503f5210f1fae6c581b
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-01-13 18:22:36 +0100

    Refactor AmountWidget and BaseWidget classes

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -8,6 +8,7 @@ class AmountWidget extends BaseWidget {
 
     thisWidget.getElements(element);
     thisWidget.initActions();
+    //console.log('AmountWidget', thisWidget);
   }
 
   getElements() {
@@ -34,8 +35,7 @@ class AmountWidget extends BaseWidget {
   initActions() {
     const thisWidget = this;
 
-    thisWidget.dom.input.addEventListener('change', function (event) {
-      event.preventDefault();
+    thisWidget.dom.input.addEventListener('change', function () {
       thisWidget.setValue(thisWidget.dom.input.value);
     });
     thisWidget.dom.linkDecrease.addEventListener('click', function (event) {

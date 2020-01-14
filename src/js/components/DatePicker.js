968704a3d567576751b422770dece06e456eca0b
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-01-14 21:11:00 +0100

    Create DatePicker.initPlugin method

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,21 +1,30 @@
 import BaseWidget from './BaseWidget';
 import utils from '../utils.js';
-import { select } from '../settings.js';
+import { select, settings } from '../settings.js';
 
 
 class DatePicker extends BaseWidget {
   constructor(wrapper) {
-
     super(wrapper, utils.dateToStr(new Date()));
     const thisWidget = this;
 
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
 
     initPlugin();
-
   }
 
+  initPlugin() {
+    const thisWidget = this;
 
+    thisWidget.minDate = new Date(thisWidget.value);
+    thisWidget.maxDate = thisWidget.minDate + settings.datePicker.maxDaysInFuture;
+
+    // TODO initiate plugin flatpickr
+  }
+
+  parseValue(value) {
+    return value;
+  }
 
 
 

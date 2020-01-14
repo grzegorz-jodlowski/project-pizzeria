fc236664ef5b36349f27eae1b37e5712f4620cd2
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-01-14 21:04:48 +0100

    Create constructor of DatePicker class

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,6 +1,21 @@
 import BaseWidget from './BaseWidget';
+import utils from '../utils.js';
+import { select } from '../settings.js';
+
 
 class DatePicker extends BaseWidget {
+  constructor(wrapper) {
+
+    super(wrapper, utils.dateToStr(new Date()));
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+
+    initPlugin();
+
+  }
+
+
 
 
 
